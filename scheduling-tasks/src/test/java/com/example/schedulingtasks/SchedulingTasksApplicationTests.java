ec1efbce63b9fb227fdecc94dd2059fdd7f2ade3
JW <jwiamx@outlook.com>
2024-04-03 14:08:46 +0900

    test: add scheduled tasks test

--- scheduling-tasks/src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTests.java
+++ scheduling-tasks/src/test/java/com/example/schedulingtasks/SchedulingTasksApplicationTests.java
@@ -1,14 +1,21 @@
 package com.example.schedulingtasks;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 public class SchedulingTasksApplicationTests {
+	@Autowired
+	private ScheduledTasks tasks;
 
 	@Test
 	public void contextLoads() {
+		// Basic integration test that shows the context starts up properly
+		assertThat(tasks).isNotNull();
 	}
 
 }
