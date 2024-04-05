3f557b4ddf2d091d3ac58cea217cd2b2ca6ebd28
JW <jwiamx@outlook.com>
2024-04-05 14:32:58 +0900

    feat: building-a-restful-web-service-with-spring-boot-actuator guide

---
+++ building-a-restful-web-service-with-spring-boot-actuator/src/test/java/com/example/actuatorservice/ActuatorServiceApplicationTests.java
@@ -0,0 +1,34 @@
+package com.example.actuatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+
+import java.util.Map;
+import static org.assertj.core.api.BDDAssertions.then;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(properties = {"management.port=0"})
+class ActuatorServiceApplicationTests {
+	@LocalServerPort
+	private int port;
+
+	@Value("${local.management.port}")
+	private int mgt;
+
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
+	public void shouldReturn200WhenSendingRequestToController() throws Exception {
+		ResponseEntity<Map> entity = this.testRestTemplate.getForEntity("http://localhost:" + this.port + "/hello-world", Map.class);
+
+		then(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
+}
