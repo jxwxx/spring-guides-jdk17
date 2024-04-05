060c474c2c7778ecf14c43ca663a6caae30123c5
JW <jwiamx@outlook.com>
2024-04-05 18:44:15 +0900

    feat: add managing-transactions guide

---
+++ managing-transactions/src/test/java/com/example/managingtransactions/ManagingTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.managingtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ManagingTransactionsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
